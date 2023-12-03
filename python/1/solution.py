e8f564f7188589935fdf59b7f4c41699b4911130
Jobava <Jobava@users.noreply.github.com>
2023-12-04 00:36:55 +0200

    Update solution.py

--- python/1/solution.py
+++ python/1/solution.py
@@ -1,24 +1,33 @@
 import re
-from copy import deepcopy
 
-with open("data.txt") as f:
-    data_lines = [line.strip() for line in f.readlines() if line.strip()]
+DIGITS = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+ALL_DIGITS = {**DIGITS, **dict((str(n),n) for n in range(10))}
 
-digits = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
-all_digits = {**digits, **dict((str(n),n) for n in range(10))}
+NUMBER_PATTERN = r"(?=(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))"
 
-number_pattern = r"(?=(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))"
+
 def get_digits(in_str):
-    matches = re.findall(number_pattern, in_str)
+    matches = re.findall(NUMBER_PATTERN, in_str)
     for m in matches:
-        yield all_digits[m]
+        yield ALL_DIGITS[m]
 
-lines_sum = 0
-for line in data_lines:
-    digits = list(get_digits(line))
-    print(line, digits)
+
+def get_number_from_line(in_line):
+    digits = list(get_digits(in_line))
     first_digit = int(digits[0])
     last_digit = int(digits[-1])
-    lines_sum += first_digit * 10 + last_digit
+    return first_digit * 10 + last_digit
 
-print(lines_sum)
+
+def get_number():
+    lines_sum = 0
+    with open("data.txt") as f:
+        for line in f:
+            if not line:
+                continue
+            yield get_number_from_line(line)
+
+
+if __name__ == "__main__":
+    solution = sum(get_number())
+    print(solution)
